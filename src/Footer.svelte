14f81ea81d7662adb4e49f8d8dacd6b25268ac3e
Sameer Tariq <webdevcrow@gmail.com>
2019-05-29 18:10:23 +0500

    Improved accessibility...

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -5,7 +5,7 @@
 </style>
 
 <footer class="footer is-black">
-    <div class="content has-text-centered">
+    <div class="content has-text-centered has-text-white">
         created by: sameer tariq &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;username: @imsamtar
     </div>
 </footer>
